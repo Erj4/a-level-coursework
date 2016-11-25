50c59ca8b86a72bfa3a544c6e5bdabc82f019351
Erj4 <edward1999.jones@gmail.com>
2016-11-25 14:34:39 +0000

    Added styled searchbox to fxml and added field to controller

--- src/erj4/as/IndexController.java
+++ src/erj4/as/IndexController.java
@@ -2,6 +2,9 @@ package erj4.as;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class IndexController extends SuperController{
+	@FXML
+	TextField searchBox;
 }
